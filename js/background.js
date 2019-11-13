c132f0d5d2faa7e2e67dd33a18120a21c852064e
Katou Fuushi <no1fushi@live.jp>
2019-11-14 03:19:08 +0900

    add tabsfunc

--- js/background.js
+++ js/background.js
@@ -1,11 +1,23 @@
-function onCreated() {
-  if (chrome.runtime.lastError) {
+// Function definition
+const onCreated = () => {
+  if(chrome.runtime.lastError) {
     console.log(`Error: ${chrome.runtime.lastError}`);
   } else {
     console.log("Item created successfully");
   }
 }
 
+const logTabs = tabs => {
+  for(let tab of tabs) console.log(tab.url);
+}
+
+const getTabs = () => {
+  chrome.tabs.query({currentWindow: true}, (tabs) => {
+    logTabs(tabs);
+  });
+}
+
+// Create contextmenus
 chrome.contextMenus.create({
   id: "getTabList",
   title: "GetTabList",
@@ -24,5 +36,15 @@ chrome.contextMenus.create({
   contexts: ["all"]
 }, onCreated);
 
+
+// Event listener
 chrome.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "getTabList":
+      getTabs();
+      break;
+    case "getTabList2":
+      getTabs();
+      break;
+  }
 });
