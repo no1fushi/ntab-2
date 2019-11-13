84c46894c63d52b39a0b059236fe5b5c423bf0ea
no1fushi <no1fushi@live.jp>
2019-11-13 09:33:57 +0900

    background.js sample

--- js/background.js
+++ js/background.js
@@ -1,15 +1,48 @@
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
 
-browser.contextMenus.create({
-	id: "log-selection",
-	title: browser.i18n.getMessage("contextMenuItemSelectionLogger"),
-	contexts: ["selection"]
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+browser.menus.create({
+  id: "separator-1",
+  type: "separator",
+  contexts: ["all"]
 }, onCreated);
 
-browser.contextMenus.onClicked.addListener(function(info, tab) {
-	switch (info.menuItemId) {
-	case "log-selection":
-	console.log(info.selectionText);
-	break;
-	}
-})
+browser.menus.create({
+  id: "separator-2",
+  type: "separator",
+  contexts: ["all"]
+}, onCreated);
 
+var checkedState = true;
+
+/*
+Toggle checkedState, and update the menu item's title
+appropriately.
+Note that we should not have to maintain checkedState independently like
+this, but have to because Firefox does not currently pass the "checked"
+property into the event listener.
+*/
+function updateCheckUncheck() {
+  checkedState = !checkedState;
+  if (checkedState) {
+    browser.menus.update("check-uncheck", {
+      title: browser.i18n.getMessage("menuItemUncheckMe"),
+    });
+  } else {
+    browser.menus.update("check-uncheck", {
+      title: browser.i18n.getMessage("menuItemCheckMe"),
+    });
+  }
+}
+
+browser.menus.onClicked.addListener((info, tab) => {
+});
