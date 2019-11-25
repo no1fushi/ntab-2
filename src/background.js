d9f82fabdfcce04055dfc0be54a827516cbfa940
Katou Fuushi <no1fushi@live.jp>
2019-11-25 15:18:04 +0900

    add bookmarks

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 // Function definition
 const onCreated = () => {
   if(chrome.runtime.lastError) {
-    console.log(`Error: ${chrome.runtime.lastError}`);
+    console.warn(`Error: ${chrome.runtime.lastError}`);
   } else {
     console.log("Item created successfully");
   }
@@ -38,45 +38,33 @@ const delTabs = (target, tabs) => {
   chrome.tabs.remove(target, () => console.log("RemoveTabs"));
 }
 
-const callOpe = ope => {
-  chrome.tabs.query({currentWindow: true}, tabs => {
-    outputTabs(tabs);
-    const newtab = countNewtab(tabs);
-
-    if(newtab.length >= 2) {
-      console.log("newtab.length :", newtab.length);
-      let target = new Array();
-      for(let index in tabs) if(index >= newtab[0] && index < newtab[1]) target.push(tabs[index].id);
-      console.log("↓ target[]");
-      console.log(target);
-
-      switch (ope) {
-        case "del":
-        delTabs(target, tabs);
-        break;
-        case "bm":
-        //delTabs(target, tabs);
-        break;
-      }
-
-    } else {
-      console.warn("There are less than 2 new tabs open");
-      console.log("newtab.length :", newtab.length);
+const bmTabs = (target, tabs) => {
+  for(let tab of tabs) {
+    if(target.includes(tab.id) && tab.id != target[0]) {
+      chrome.bookmarks.create({
+        title: tab.title,
+        url: tab.url
+      }, bookmarkItem => console.log("Bookmark added with ID: " + bookmarkItem.id));
     }
-
-  });
+  }
 }
 
 // Create contextmenus
 chrome.contextMenus.create({
-  id: "delTabs",
-  title: "delTabs",
+  id: "del",
+  title: "delete Tabs",
   contexts: ["all"]
 }, onCreated);
 
 chrome.contextMenus.create({
-  id: "getTabList2",
-  title: "GetTabList2",
+  id: "bm",
+  title: "bookmark Tabs",
+  contexts: ["all"]
+}, onCreated);
+
+chrome.contextMenus.create({
+  id: "bm-del",
+  title: "bookmark and delete Tabs",
   contexts: ["all"]
 }, onCreated);
 
@@ -88,12 +76,35 @@ chrome.contextMenus.create({
 
 // Event listener
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  switch (info.menuItemId) {
-    case "delTabs":
-    callOpe("del");
-    break;
-    case "getTabList2":
-    callOpe("bm");
-    break;
-  }
+  chrome.tabs.query({currentWindow: true}, tabs => {
+
+    outputTabs(tabs);
+    const newtab = countNewtab(tabs);
+    console.log("Number of new tabs open :", newtab.length);
+
+    if(newtab.length >= 2) {
+      
+      let target = new Array();
+      for(let index in tabs) if(index >= newtab[0] && index < newtab[1]) target.push(tabs[index].id);
+      console.log("↓ target[]");
+      console.log(target);
+
+      switch (info.menuItemId) {
+        case "del":
+        delTabs(target, tabs);
+        break;
+        case "bm":
+        bmTabs(target, tabs);
+        break;
+        case "bm-del":
+        bmTabs(target, tabs);
+        delTabs(target, tabs);
+        break;
+      }
+
+    } else {
+      console.warn("There are less than 2 new tabs open");
+    }
+
+  });
 });
